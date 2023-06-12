b95291adaff15687ee56e260892813269a8e8c85
root <Prosper Mawuli Auayigah>
2023-06-12 11:22:49 -0700

    python data

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,25 +1,23 @@
-#ifndef LISTS_H
-#define LISTS_H
+#include "lists.h"
 
-#include <stdlib.h>
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for project
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ * Return: pointer to the first node in the new list
  */
-typedef struct listint_s
+void reverse_listint(listint_t **head)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
 
-int is_palindrome(listint_t **head);
-
-#endif /* LISTS_H */
+	*head = prev;
+}
